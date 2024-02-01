d7d49a94a5b4427bfde9b6714e7355aa7f2bb3ce
Egor Zudin <zudin.0197@gmail.com>
2024-02-01 06:00:31 +0300

    Add bench-runner script

---
+++ run_benches.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3
+
+import argparse
+import os
+import random
+import string
+import subprocess
+
+parser = argparse.ArgumentParser(prog="run_benches")
+parser.add_argument("--tbb-path", type=str, help="path to directory with tbb .so and /include")
+
+parser.add_argument("--omp", action=argparse.BooleanOptionalAction, help="set if want to run with OpenMP executor")
+parser.add_argument("--tbb", action=argparse.BooleanOptionalAction, help="set if want to run with oneTBB executor")
+parser.add_argument("--eigen", action=argparse.BooleanOptionalAction, help="set if want to run with eigen executor")
+
+parser.add_argument("--numa", action=argparse.BooleanOptionalAction, help="set if run on numa machine")
+parser.add_argument("--only", type=str, help="only run given test")
+parser.add_argument("--small", action=argparse.BooleanOptionalAction, help="scjet if want to test on smaller datasets")
+parser.add_help = True
+
+args = parser.parse_args()
+
+if args.tbb and not args.tbb_path:
+    print("if you want to run openTBB tests, --tbb-path is expected")
+    exit(1)
+
+print(args)
+
+class Executor:
+    def __init__(self, name, flag, modes):
+        self.name = name
+        self.flag = flag
+        self.modes = modes
+
+
+executors = []
+
+if args.omp:
+    omp_modes = [
+        "OMP_STATIC",
+        "OMP_DYNAMIC_MONOTONIC",
+        "OMP_DYNAMIC_NONMONOTONIC",
+        "OMP_GUIDED_MONOTONIC",
+        "OMP_GUIDED_NONMONOTONIC",
+    ]
+    omp_executor = Executor(name="omp", flag=("OPENMP", 1), modes=omp_modes)
+
+    executors.append(omp_executor)
+
+if args.tbb:
+    tbb_modes = [
+        "TBB_SIMPLE",
+        "TBB_AUTO",
+        "TBB_AFFINITY",
+        "TBB_CONST_AFFINITY",
+    ]
+    tbb_executor = Executor(name="tbb", flag=("TBB_PATH", args.tbb_path), modes=tbb_modes)
+
+    executors.append(tbb_executor)
+
+if args.eigen:
+    eigen_modes = [
+        "EIGEN_SIMPLE",
+        "EIGEN_TIMESPAN",
+        "EIGEN_STATIC",
+        "EIGEN_TIMESPAN_GRAINSIZE",
+    ]
+    eigen_executor = Executor(name="eigen", flag=("EIGEN", 1), modes=eigen_modes)
+
+    executors.append(eigen_executor)
+
+def random_str(length=8):
+    return "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
+
+target_dir = "bench_res_" + random_str()
+os.makedirs(target_dir)
+print(f"target dir for this run is {target_dir}")
+
+for executor in executors:
+    flag, value = executor.flag
+    exec_env = os.environ.copy()
+    exec_env[flag] = str(value)
+
+    mode_name = f"{executor.name.upper()}_MODE"
+    for mode in executor.modes:
+        run_env = exec_env.copy()
+        run_env[mode_name] = mode
+        print(f"now {executor.name} : {mode}, env: {run_env}")
+
+        output_path = f"{target_dir}/{mode}.txt"
+        output_file = open(output_path, "w")
+
+        cmd = ["./runall", "-force", "-par"]
+
+        if not args.numa:
+            cmd.append("-nonuma")
+        if args.only:
+            cmd.extend(["-only", args.only])
+        if args.small:
+            cmd.extend(["-small"])
+
+        process = subprocess.Popen(cmd, env=run_env, universal_newlines=True, stdout=output_file)
+        rc = process.wait()
+        if rc != 0:
+            print(f"{mode_name} had bad returncode: {rc}")
