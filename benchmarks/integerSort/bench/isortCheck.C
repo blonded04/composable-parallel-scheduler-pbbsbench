5adbe1b30eba30207c16690084a93096addfe51c
Egor Zudin <zudin.0197@gmail.com>
2024-02-02 14:48:20 +0300

    Speedup input

--- benchmarks/integerSort/bench/isortCheck.C
+++ benchmarks/integerSort/bench/isortCheck.C
@@ -20,6 +20,8 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+#include <charconv>
+#include <filesystem>
 #include <iostream>
 #include <algorithm>
 #include <cstring>
@@ -32,16 +34,16 @@ using namespace std;
 using namespace benchIO;
 
 template <class T, class LESS>
-void checkSort(sequence<sequence<char>> In,
-	       sequence<sequence<char>> Out,
+void checkSort(sequence<T> in_vals,
+	       sequence<T> out_vals,
 	       LESS less) {
-  sequence<T> in_vals = parseElements<T>(In.cut(1, In.size()));
-  sequence<T> out_vals = parseElements<T>(Out.cut(1, In.size()));
   size_t n = in_vals.size();
+  // std::stable_sort(in_vals.begin(), in_vals.end(), less);
+  // auto sorted_in = in_vals;
   auto sorted_in = parlay::stable_sort(in_vals, less);
   size_t error = n;
   parlay::parallel_for (0, n, [&] (size_t i) {
-      if (out_vals[i] != sorted_in[i]) 
+      if (out_vals[i] != sorted_in[i])
 	pbbs::write_min(&error,i,std::less<size_t>());
   });
   if (error < n) {
@@ -53,19 +55,76 @@ void checkSort(sequence<sequence<char>> In,
   }
 }
 
+std::string readFile(std::string filenameStr) {
+  std::filesystem::path filename{filenameStr};
+  auto fileSize = std::filesystem::file_size(filename);
+  std::string content(fileSize, '\0');
+
+  std::ifstream file{filenameStr};
+  if (!file.read(content.data(), fileSize)) {
+    std::cerr << "I CAN'T READ" << std::endl;
+    throw std::runtime_error{"I CAN'T READ"};
+  }
+
+  return content;
+}
+
+struct Tokens {
+  std::string content;
+  std::vector<std::string_view> parts;
+};
+
+Tokens readTokens(std::string filenameStr) {
+  Tokens tokensRes{.content = readFile(std::move(filenameStr))};
+
+  std::string_view contentView = tokensRes.content;
+  size_t lastNewLine = 0;
+  for (auto newLine = contentView.find_first_of("\n "); newLine != std::string_view::npos; newLine = contentView.find_first_of("\n ", newLine + 1)) {
+    size_t tokenStart = lastNewLine;
+    size_t tokenSize = newLine - tokenStart;
+    auto token = contentView.substr(tokenStart, tokenSize);
+    tokensRes.parts.push_back(token);
+    lastNewLine = newLine + 1;
+  }
+  return tokensRes;
+}
+
+template <class T>
+T parseValue(std::string_view input) {
+  T value{};
+  auto res = std::from_chars(input.data(), input.data() + input.size(), value);
+  if (res.ec != std::errc{}) {
+    throw std::runtime_error{std::string{"couldn't parse value: "} + std::string{input}};
+  }
+  return value;
+}
+
+template <class T, class It>
+sequence<T> parseValues(It begin, It end) {
+  auto size = std::distance(begin, end);
+  if constexpr (std::is_same_v<T, uint>){
+    return parlay::tabulate(size, [&begin](size_t i) { return parseValue<uint>(*(begin + i)); });
+  } else if constexpr (std::is_same_v<T, uintPair>) {
+    return tabulate(size/2, [&] (long i) -> uintPair {
+        return std::make_pair((uint) parseValue<uint>(*(begin + 2*i)), parseValue<uint>(*(begin + 2*i+1)));});
+  } else {
+    static_assert(std::is_same_v<T, uint>);
+  }
+}
+
 int main(int argc, char* argv[]) {
   commandLine P(argc,argv,"<inFile> <outFile>");
   pair<char*,char*> fnames = P.IOFileNames();
   char* infile = fnames.first;
   char* outfile = fnames.second;
   
-  auto In = get_tokens(infile);
-  elementType in_type = elementTypeFromHeader(In[0]);
-  size_t in_n = In.size() - 1;
+  auto myIn = readTokens(infile);
+  elementType in_type = elementTypeFromHeader(myIn.parts[0]);
+  size_t in_n = myIn.parts.size() - 1; // 126528
 
-  auto Out = get_tokens(outfile);
-  elementType out_type = elementTypeFromHeader(Out[0]);
-  size_t out_n = In.size() - 1;
+  auto myOut = readTokens(outfile);
+  elementType out_type = elementTypeFromHeader(myOut.parts[0]);
+  size_t out_n = myOut.parts.size() - 1; // 126528
 
   if (in_type != out_type) {
     cout << argv[0] << ": in and out types don't match" << endl;
@@ -77,16 +136,22 @@ int main(int argc, char* argv[]) {
     return(1);
   }
 
-  auto less = [&] (uint a, uint b) {return a < b;};
+  auto less = std::less<uint>{};
   auto lessp = [&] (uintPair a, uintPair b) {return a.first < b.first;};
   
   switch (in_type) {
-  case intType: 
-    checkSort<uint>(In, Out, less);
-    break; 
-  case intPairT: 
-    checkSort<uintPair>(In, Out, lessp);
-    break; 
+  case intType: {
+    auto inValues = parseValues<uint>(myIn.parts.begin() + 1, myIn.parts.end());
+    auto outValues = parseValues<uint>(myOut.parts.begin() + 1, myOut.parts.end());
+    checkSort(inValues, outValues, less);
+    break;
+  }
+  case intPairT: {
+    auto inValues = parseValues<uintPair>(myIn.parts.begin() + 1, myIn.parts.end());
+    auto outValues = parseValues<uintPair>(myOut.parts.begin() + 1, myOut.parts.end());
+    checkSort(inValues, outValues, lessp);
+    break;
+  }
   default:
     cout << argv[0] << ": input files not of right type" << endl;
     return(1);
--- benchmarks/integerSort/bench/isortTime.C
+++ benchmarks/integerSort/bench/isortTime.C
@@ -25,14 +25,15 @@
 #include "common/time_loop.h"
 #include "common/parse_command_line.h"
 #include "common/sequenceIO.h"
+#include <charconv>
+#include <filesystem>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 using namespace benchIO;
 
 template <class T>
-void timeIntegerSort(sequence<sequence<char>> In, int rounds, int bits, char* outFile) {
-  auto in_vals = parseElements<T>(In.cut(1, In.size()));
+void timeIntegerSort(sequence<T> in_vals, int rounds, int bits, char* outFile) {
   size_t n = in_vals.size();
   sequence<T> R;
   time_loop(rounds, 1.0,
@@ -42,6 +43,64 @@ void timeIntegerSort(sequence<sequence<char>> In, int rounds, int bits, char* ou
   if (outFile != NULL) writeSequenceToFile(R, outFile);
 }
 
+std::string readFile(std::string filenameStr) {
+  std::filesystem::path filename{filenameStr};
+  auto fileSize = std::filesystem::file_size(filename);
+  std::string content(fileSize, '\0');
+  std::cout << "Now have string of size " << content.size() << std::endl;
+
+  std::ifstream file{filenameStr};
+  if (!file.read(content.data(), fileSize)) {
+    std::cerr << "I CAN'T READ" << std::endl;
+    throw std::runtime_error{"I CAN'T READ"};
+  }
+
+  return content;
+}
+
+struct Tokens {
+  std::string content;
+  std::vector<std::string_view> parts;
+};
+
+Tokens readTokens(std::string filenameStr) {
+  Tokens tokensRes{.content = readFile(std::move(filenameStr))};
+
+  std::string_view contentView = tokensRes.content;
+  size_t lastNewLine = 0;
+  for (auto newLine = contentView.find_first_of("\n "); newLine != std::string_view::npos; newLine = contentView.find_first_of("\n ", newLine + 1)) {
+    size_t tokenStart = lastNewLine;
+    size_t tokenSize = newLine - tokenStart;
+    auto token = contentView.substr(tokenStart, tokenSize);
+    tokensRes.parts.push_back(token);
+    lastNewLine = newLine + 1;
+  }
+  return tokensRes;
+}
+
+template <class T>
+T parseValue(std::string_view input) {
+  T value{};
+  auto res = std::from_chars(input.data(), input.data() + input.size(), value);
+  if (res.ec != std::errc{}) {
+    throw std::runtime_error{std::string{"couldn't parse value: "} + std::string{input}};
+  }
+  return value;
+}
+
+template <class T, class It>
+sequence<T> parseValues(It begin, It end) {
+  auto size = std::distance(begin, end);
+  if constexpr (std::is_same_v<T, uint>){
+    return parlay::tabulate(size, [&begin](size_t i) { return parseValue<uint>(*(begin + i)); });
+  } else if constexpr (std::is_same_v<T, uintPair>) {
+    return tabulate(size/2, [&] (long i) -> uintPair {
+        return std::make_pair((uint) parseValue<uint>(*(begin + 2*i)), parseValue<uint>(*(begin + 2*i+1)));});
+  } else {
+    static_assert(std::is_same_v<T, uint>);
+  }
+}
+
 int main(int argc, char* argv[]) {
   commandLine P(argc,argv,"[-o <outFile>] [-r <rounds>] <inFile>");
   char* iFile = P.getArgument(0);
@@ -49,17 +108,23 @@ int main(int argc, char* argv[]) {
   int rounds = P.getOptionIntValue("-r",1);
   int bits = P.getOptionIntValue("-b",0);
 
-  auto In = get_tokens(iFile);
-  elementType in_type = elementTypeFromHeader(In[0]);
+  auto myIn = readTokens(iFile);
+  elementType in_type = elementTypeFromHeader(myIn.parts[0]);
   cout << "bits = " << bits << endl;
 
   switch (in_type) {
-  case intType: 
-    timeIntegerSort<uint>(In, rounds, bits, oFile);
+  case intType: {
+    auto inValues = parseValues<uint>(myIn.parts.begin() + 1, myIn.parts.end());
+    std::cout << "Parsed " << inValues.size() << " uints" << std::endl;
+    timeIntegerSort(std::move(inValues), rounds, bits, oFile);
     break;
-  case intPairT: 
-    timeIntegerSort<uintPair>(In, rounds, bits, oFile);
+  }
+  case intPairT: {
+    auto inValues = parseValues<uintPair>(myIn.parts.begin() + 1, myIn.parts.end());
+    std::cout << "Parsed " << inValues.size() << " pairs" << std::endl;
+    timeIntegerSort(std::move(inValues), rounds, bits, oFile);
     break;
+  }
   default:
     cout << "integer Sort: input file not of right type" << endl;
     return(1);
