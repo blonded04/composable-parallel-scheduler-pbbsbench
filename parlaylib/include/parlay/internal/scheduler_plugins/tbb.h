b1098cf31dd9358048ad70e11ed25dfc1eb631c6
Egor Zudin <zudin.0197@gmail.com>
2024-05-14 19:32:00 +0300

    Make tbb correct

--- parlaylib/include/parlay/internal/scheduler_plugins/tbb.h
+++ parlaylib/include/parlay/internal/scheduler_plugins/tbb.h
@@ -8,6 +8,7 @@
 #include <tbb/blocked_range.h>
 #include <tbb/parallel_for.h>
 #include <tbb/parallel_invoke.h>
+#include <tbb/partitioner.h>
 #include <tbb/task_arena.h>
 
 #include "common/tbb_pinner.h"
@@ -27,54 +28,36 @@ inline size_t worker_id() {
 template <typename F>
 inline void parallel_for(size_t start, size_t end, F&& f, long granularity, bool) {
   static_assert(std::is_invocable_v<F&, size_t>);
-  // Use TBB's automatic granularity partitioner (tbb::auto_partitioner)
-  if (granularity == 0) {
-    tbb::parallel_for(tbb::blocked_range<size_t>(start, end), [&](const tbb::blocked_range<size_t>& r) {
-      for (auto i = r.begin(); i != r.end(); ++i) {
-        f(i);
-      }
-    }, tbb::auto_partitioner{});
-  }
-  // Otherwise, use the granularity specified by the user (tbb::simple_partitioner)
-  else {
-    static tbb::task_group_context context(
-      tbb::task_group_context::bound,
-      tbb::task_group_context::default_traits |
-          tbb::task_group_context::concurrent_wait);
-    #if TBB_MODE == TBB_SIMPLE
-      const tbb::simple_partitioner part;
-    #elif TBB_MODE == TBB_AUTO
-      const tbb::auto_partitioner part;
-    #elif TBB_MODE == TBB_AFFINITY
-      // "it is important that the same affinity_partitioner object be passed to
-      // loop templates to be optimized for affinity" see
-      // https://spec.oneapi.io/versions/latest/elements/oneTBB/source/algorithms/partitioners/affinity_partitioner.html
-      static tbb::affinity_partitioner part;
-    #elif TBB_MODE == TBB_CONST_AFFINITY
-      tbb::affinity_partitioner part;
-    #elif TBB_MODE == TBB_RAPID
-      // no partitioner
-    #else
-      static_assert(false, "Wrong TBB_MODE mode");
-    #endif
-      // TODO: grain size?
-    #if TBB_MODE == TBB_RAPID
-      RapidGroup.parallel_ranges(start, end, [&](auto from, auto to, auto part) {
-        for (size_t i = from; i != to; ++i) {
+  static tbb::task_group_context context(
+    tbb::task_group_context::bound,
+    tbb::task_group_context::default_traits |
+        tbb::task_group_context::concurrent_wait);
+  tbb::auto_partitioner part;
+  #if TBB_MODE == TBB_SIMPLE
+    const tbb::simple_partitioner part;
+  #elif TBB_MODE == TBB_AUTO
+    const tbb::auto_partitioner part;
+  #elif TBB_MODE == TBB_AFFINITY
+    // "it is important that the same affinity_partitioner object be passed to
+    // loop templates to be optimized for affinity" see
+    // https://spec.oneapi.io/versions/latest/elements/oneTBB/source/algorithms/partitioners/affinity_partitioner.html
+    static tbb::affinity_partitioner part;
+  #elif TBB_MODE == TBB_CONST_AFFINITY
+    tbb::affinity_partitioner part;
+  #else
+    static_assert(false, "Wrong TBB_MODE mode");
+  #endif
+    // TODO: grain size?
+
+  tbb::parallel_for(
+      tbb::blocked_range(start, end, granularity),
+      [&](const tbb::blocked_range<size_t> &range) {
+        for (size_t i = range.begin(); i != range.end(); ++i) {
           f(i);
         }
-      });
-    #else
-      tbb::parallel_for(
-          tbb::blocked_range(start, end),
-          [&](const tbb::blocked_range<size_t> &range) {
-            for (size_t i = range.begin(); i != range.end(); ++i) {
-              f(i);
-            }
-          },
-          part, context);
-    #endif
-  }
+      },
+      part, context);
+
 }
 
 template <typename Lf, typename Rf>
